21d37dd892e97c4c3f49c387681d6ce4ae6d15ab
Ole Henrik Waagaard <ohwaagar@NOTRON031798.asn.com>
2022-10-05 21:11:56 +0200

    Cleaning up __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May  4 12:52:33 2021
-
-@author: ohwaagaard
-"""
-# For relative imports to work in Python 3.6
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
